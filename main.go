e6e07a992c253c1163166b18e2a6b84fc466e8f4
Damian Gerow <dwg@flargle.io>
2018-01-11 15:39:30 -0500

    Write out our interpreted logfile for future use.

--- main.go
+++ main.go
@@ -96,12 +96,32 @@ func main() {
 	// }
 	// fmt.Println(buffer.String())
 
-	fmt.Println("")
+	parsedLogFile := "ParsedWoWCombatLog.txt"
+	fh, err := os.Create(parsedLogFile)
+	if err != nil {
+		log.Printf("Failed to create prased log file: %s", err)
+	}
+	w := bufio.NewWriter(fh)
+	defer fh.Close()
+
+	_, err = w.WriteString(combatLogInfo.Header + "\n")
+	if err != nil {
+		log.Printf("Failed to write combat log header: %s", err)
+	}
+
 	for _, encounter := range encounters {
 		encounterLength := encounter.End.Sub(encounter.Start).Round(1 * time.Second)
 		encounterResult := killOrWipe(encounter.Kill)
 		fmt.Printf("%s of %s lasted %s.\n", encounterResult, encounter.Name, encounterLength)
+
+		for _, event := range encounter.Events {
+			_, err = w.WriteString(event + "\n")
+			if err != nil {
+				log.Printf("Failed to write combat event: %s", err)
+			}
+		}
 	}
+
 }
 
 func killOrWipe(k bool) string {
